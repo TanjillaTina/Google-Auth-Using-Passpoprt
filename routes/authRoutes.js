e7d27320c5c79fbd60fb94ad957f78ecc97fb389
Tina <tanjilla.tina@gmail.com>
2018-10-03 19:02:41 +0600

    Initial commit

---
+++ routes/authRoutes.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+const passport=require('passport');
+
+/* GET users listing. */
+
+//auth login
+router.get('/login', (req, res)=> {
+  res.render('login');
+});
+
+//auth logout
+router.get('/logout',(req, res)=>{
+  res.send('respond with a logout');
+});
+
+//// auth with google
+router.get('/google', passport.authenticate('google',{
+  scope:['profile']
+}));
+
+
+//callback route for gogle to redirect to
+router.get('/google/redirect',passport.authenticate('google'),(req, res)=>{
+
+  ///accesing currently loggedin user
+  res.send(req.user);
+  //res.send('Reached to callback');
+});
+
+
+module.exports = router;
