d61e12468d40667493d31b48024d87745988385c
Tina <tanjilla.tina@gmail.com>
2018-10-03 19:29:09 +0600

    checking if logged in is done

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,7 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+//this fun is to check if,someone is logged-in in the page, if yes, redirect to profile page, else redirect to login page
+//middleware function, that's gonna set in before profile page is redirected
+const authCheck=(req,res,next)=>{
+   if(!req.user){
+     //if user isn't logged in 
+     res.redirect('/auth/login');
+
+   }
+   else{
+    res.send('Logged In with Profile '+req.user.username);
+   }
+};
+
+
 router.get('/', (req, res)=> {
   //res.render('profile');
   res.send('Logged In with Profile '+req.user.username);
